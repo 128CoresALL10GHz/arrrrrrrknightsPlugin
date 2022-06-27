d243fdebfa6525d6095117165f29546867da9306
GitHub Action <action@github.com>
2022-06-27 03:07:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/128coresall10ghz/arrrrrrrknightsplugin/MyBundle.kt
+++ src/main/kotlin/com/github/128coresall10ghz/arrrrrrrknightsplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.128coresall10ghz.arrrrrrrknightsplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/128coresall10ghz/arrrrrrrknightsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.128coresall10ghz.arrrrrrrknightsplugin.services
+
+import com.github.128coresall10ghz.arrrrrrrknightsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
